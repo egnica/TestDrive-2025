6b40e8fc996adefeeaa05e8c1cb68ebc44bf9f08
egnica <46502133+egnica@users.noreply.github.com>
2025-02-17 22:18:56 -0600

    working on bank display

--- app/components/main_three/Banks.jsx
+++ app/components/main_three/Banks.jsx
@@ -1,7 +1,34 @@
+"use client";
 import React from "react";
-
+import ObjectBank from "../../../banks.json";
+import { useState } from "react";
 const Banks = () => {
-  return <div>Banks</div>;
+  const [bank, setBank] = useState(null);
+  return (
+    <>
+      <div>Banks</div>
+      {Object.entries(ObjectBank.bank_layout).map(([key, value], index) => {
+        return (
+          <div key={index}>
+            <p onClick={() => setBank(value.bank_name)}>{value.bank_name}</p>
+          </div>
+        );
+      })}
+      <hr />
+      <p>{bank}</p>
+      {Object.entries(ObjectBank.bank_layout)
+        .filter(([key, value]) => value.bank_name === bank)
+        .map(([key, value2]) =>
+          value2.categorys.map(({ name, score, features }, index) => (
+            <div key={index}>
+              <p>
+                {name}: {score}
+              </p>
+            </div>
+          ))
+        )}
+    </>
+  );
 };
 
 export default Banks;
