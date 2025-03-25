f62ee09230a6def3e2ba1045dc4f2882e6567d78
egnica <46502133+egnica@users.noreply.github.com>
2025-03-25 08:33:14 -0500

    bank section

--- app/components/main_three/Banks.jsx
+++ app/components/main_three/Banks.jsx
@@ -2,11 +2,22 @@
 import React from "react";
 import ObjectBank from "../../../banks.json";
 import Feature from "../bank-comp/Feature";
-import { useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import styles from "../page.module.css";
+import Image from "next/image";
 const Banks = () => {
   const [bank, setBank] = useState(null);
   const [feature, setFeature] = useState(null);
+
+  const [contentHeight, setContentHeight] = useState(0);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setContentHeight(contentRef.current.scrollHeight);
+    }
+  }, [feature]); //
 
   const bankHandler = (bankName) => {
     bankName === bank ? setBank(null) : setBank(bankName);
@@ -15,36 +26,116 @@ const Banks = () => {
   const featureHandler = (index) => {
     feature === index ? setFeature(null) : setFeature(index);
   };
+
+  const boxVariants = {
+    hidden: {
+      opacity: 0,
+      scale: 0.5,
+      transition: { duration: 0.3, ease: "easeOut" },
+    },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      borderRadius: "8px",
+      transition: {
+        type: "spring",
+        stiffness: 200,
+        damping: 15,
+        mass: 1,
+        bounce: 0.3,
+        restSpeed: 0.01,
+        restDelta: 0.01,
+      },
+    },
+    hover: {
+      scale: 1.05,
+      transition: { type: "spring", stiffness: 300 },
+      borderRadius: "12px",
+      boxShadow: "0px 4px 18px rgb(192, 185, 255)",
+      cursor: "pointer",
+    },
+    tap: {
+      scale: 0.9,
+      transition: { duration: 0.3 },
+      backgroundColor: "rgb(238, 236, 254)",
+    },
+    selected: {
+      opacity: 1,
+      scale: 1.05,
+      borderRadius: "12px",
+      boxShadow: "0px 4px 8px rgb(192, 185, 255)",
+      transition: { duration: 0.3, stiffness: 300 },
+    },
+  };
+
   return (
     <>
-      <div>Banks</div>
-      {Object.entries(ObjectBank.bank_layout).map(([_, value], index) => {
-        return (
-          <div key={index}>
-            <p onClick={() => bankHandler(value.bank_name)}>
-              {value.bank_name}
-            </p>
-          </div>
-        );
-      })}
+      <div style={{ textAlign: "center" }}>
+        <h2 style={{ textAlign: "center" }}>Banks</h2>
+        <p>
+          Select one of the logos below to see how that institution scored this
+          year.
+        </p>
+      </div>
+      <div className={styles.bankSelect}>
+        {Object.entries(ObjectBank.bank_layout).map(([_, value], index) => {
+          return (
+            <a
+              href="#feature"
+              style={{ textDecoration: "none", color: "inherit" }}
+              key={index}
+            >
+              <motion.div
+                onClick={() => bankHandler(value.bank_name)}
+                className={styles.bankItem}
+                key={index}
+                variants={boxVariants}
+                initial="hidden"
+                animate={bank === value.bank_name ? "selected" : "visible"}
+                whileHover="hover"
+                whileTap="tap"
+              >
+                <Image
+                  alt={value.bank_name}
+                  height={75}
+                  width={75}
+                  src={value.logo}
+                />
+                <p>{value.bank_name}</p>
+              </motion.div>
+            </a>
+          );
+        })}
+      </div>
       <hr />
-      <p>{bank}</p>
+      <motion.h2 id="feature">{bank}</motion.h2>
       {Object.entries(ObjectBank.bank_layout)
         .filter(([key, value]) => value.bank_name === bank)
         .map(([key, value2]) =>
           value2.categorys.map(({ name, score, features }, index) => (
-            <div key={index} onClick={() => featureHandler(index)}>
-              <p>
-                {name}: {score}
-              </p>
-              <AnimatePresence>
-                {index === feature ? (
-                  <motion.div>
-                    <Feature feature={features} />
-                  </motion.div>
-                ) : null}
-              </AnimatePresence>
-            </div>
+            <AnimatePresence mode="wait" key={index}>
+              <motion.div key={name} onClick={() => featureHandler(index)}>
+                <p>
+                  {name}: {score}
+                </p>
+                <AnimatePresence mode="wait">
+                  {index === feature && (
+                    <motion.div
+                      key="feature-detail"
+                      initial={{ height: 0, opacity: 0 }}
+                      animate={{ height: contentHeight, opacity: 1 }}
+                      exit={{ height: 0, opacity: 0 }}
+                      transition={{ duration: 0.4, ease: "easeInOut" }}
+                      style={{ overflow: "hidden" }}
+                    >
+                      <div ref={contentRef}>
+                        <Feature feature={features} />
+                      </div>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </motion.div>
+            </AnimatePresence>
           ))
         )}
     </>
