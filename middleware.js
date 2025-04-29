51e0e78130c819e5ae9e2a51977e1903a4cc3ea7
egnica <46502133+egnica@users.noreply.github.com>
2025-04-29 13:03:07 -0500

    PHPSESSID not working... took out the middleware

--- middleware.js
+++ middleware.js
@@ -5,12 +5,12 @@ export function middleware(req) {
 
   console.log(cookie);
 
-  if (!cookie) {
-    // If no PHPSESSID cookie, redirect to login page
-    return NextResponse.redirect(
-      new URL("https://mybarlow.barlowresearch.com/login.php")
-    );
-  }
+  // if (!cookie) {
+  //   // If no PHPSESSID cookie, redirect to login page
+  //   return NextResponse.redirect(
+  //     new URL("https://mybarlow.barlowresearch.com/login.php")
+  //   );
+  // }
 
   // If cookie exists, allow to continue
   return NextResponse.next();
