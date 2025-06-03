f4bad065d855aea376b455e78fd6abe2bcdd53fd
egnica <46502133+egnica@users.noreply.github.com>
2025-06-03 14:20:37 -0500

    added global to nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["mybarlow.barlowresearch.com", "barlowresearch.com"],
   },
+  env: {
+    FILEMAKER_API_USERNAME: process.env.FILEMAKER_API_USERNAME,
+    FILEMAKER_API_PASSWORD: process.env.FILEMAKER_API_PASSWORD,
+  },
 };
 
 export default nextConfig;
